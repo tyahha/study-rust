d07981da2953508eb0bbf57079c9fdc5d0bebc49
toshinori.sato <tyahhahha@gmail.com>
2023-10-19 22:49:47 +0900

    数当てゲーム

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
+    println!("Guess the number!");
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(&message, width, &mut writer).unwrap();
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim() {
+            "quit" => break,
+            s => match s.parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            },
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            },
+        }
+    }
 }
