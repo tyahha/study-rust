56875c8fecad95c217965703c1f217851f419b69
toshinori.sato <tyahhahha@gmail.com>
2023-10-21 23:43:47 +0900

    sample that index out of bounds error can occur

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 fn main() {
-    let tup = (500, 6.4, 1);
+    let a = [1,2,3,4,5];
 
-    let (_, y, _) = tup;
+    println!("Please enter an array index");
 
-    println!("The value of y is: {}", y);
-    println!("The tuple values: {:?}", tup);
+    let mut index = String::new();
+
+    std::io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+
+    let index: usize = index.trim().parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {} is: {}", index, element);
 }
