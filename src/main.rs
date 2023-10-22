6a4a51a9b7b8685f98c833390f71198ff01abd76
toshinori.sato <tyahhahha@gmail.com>
2023-10-22 22:01:23 +0900

    loop and break sample

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,20 @@
 fn main() {
-    let condition = true;
-    let number = if condition { 5 } else { 6 };
+    let mut count = 0;
+    'counting_up: loop {
+        println!("count = {}", count);
+        let mut remaining = 10;
 
-    println!("The value of number is: {}", number);
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
 }
