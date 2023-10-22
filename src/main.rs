4c8804daf9fabb559ba42bfadd7e72d394d3501e
toshinori.sato <tyahhahha@gmail.com>
2023-10-22 21:56:30 +0900

    if formula sample

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 fn main() {
-    let number = 3;
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
 
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
-    }
+    println!("The value of number is: {}", number);
 }
