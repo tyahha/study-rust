5e4d0a73af603581a4f54162efbbc1e277a77c97
toshinori.sato <tyahhahha@gmail.com>
2023-10-22 21:30:50 +0900

    function sample

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,9 @@
 fn main() {
-    let a = [1,2,3,4,5];
+    println!(" Hello, world!");
 
-    println!("Please enter an array index");
+    another_function();
+}
 
-    let mut index = String::new();
-
-    std::io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
-
-    let index: usize = index.trim().parse()
-        .expect("Index entered was not a number");
-
-    let element = a[index];
-
-    println!("The value of the element at index {} is: {}", index, element);
+fn another_function() {
+    println!("Another function");
 }
