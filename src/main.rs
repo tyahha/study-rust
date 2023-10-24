85651f34b8938ffc3f684c98167f3b95c55bf544
toshinori.sato <tyahhahha@gmail.com>
2023-10-24 21:46:20 +0900

    references sample

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
 fn main() {
-    let s = String::from("hello");
+    let s1 = String::from("hello");
 
-    let s = takes_ownership(s);
+    let len = calculate_length(&s1);
 
-    println!("{}", s);
-
-    let x = 5;
-
-    makes_copy(x);
+    println!("The length of '{}' is {}.", s1, len);
 }
 
-fn takes_ownership(some_string: String) -> String {
-    println!("{}", some_string);
-    some_string
-}
-
-fn makes_copy(some_integer: i32) {
-    println!("{}", some_integer);
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
