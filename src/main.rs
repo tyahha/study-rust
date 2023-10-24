f9a40464890824757be63ec767a5afe6eb67789a
toshinori.sato <tyahhahha@gmail.com>
2023-10-24 21:49:57 +0900

    mutable reference sample

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 fn main() {
-    let s1 = String::from("hello");
+    let mut s = String::from("hello");
 
-    let len = calculate_length(&s1);
+    change(&mut s);
 
-    println!("The length of '{}' is {}.", s1, len);
+    println!("{}", s);
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
