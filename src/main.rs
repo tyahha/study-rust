1a1e34c9436d29abb691887efd8c321bd58a603b
toshinori.sato <tyahhahha@gmail.com>
2023-10-25 21:52:55 +0900

    Comparison unit-like structs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 #[derive(Debug)]
+#[derive(PartialEq)]
 struct AlwaysEqual;
+#[derive(Debug)]
+#[derive(PartialEq)]
+struct AlwaysEqual2;
 
 fn main() {
-    let subject = AlwaysEqual;
-    println!("subject: {:?}", subject);
+    let subject1 = AlwaysEqual;
+    let subject2 = AlwaysEqual;
+    let subject3 = AlwaysEqual2;
+    println!("subject1: {:?}", subject1);
+    println!("subject2: {:?}", subject2);
+    println!("subject3: {:?}", subject3);
+    println!("subject1 == subject2: {}", subject1 == subject2);
+    // println!("subject1 == subject3: {}", subject1 == subject3); // compile error
 }
