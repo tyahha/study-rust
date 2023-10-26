775e8413b31d18f8bed16c5855160ffdd82a2bed
toshinori.sato <tyahhahha@gmail.com>
2023-10-26 22:20:06 +0900

    method syntax

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,14 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
 
-    println!("The area of the rect angle is {} square pixels", area(&rect1))
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("The area of the rectangle is {}", rect1.area());
 }
