d593dd8349cdc4ad0cda0baad994dc5f877258cc
toshinori.sato <tyahhahha@gmail.com>
2023-10-26 22:12:35 +0900

    Using struct sample

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
-#[derive(Debug)]
-#[derive(PartialEq)]
-struct AlwaysEqual;
-#[derive(Debug)]
-#[derive(PartialEq)]
-struct AlwaysEqual2;
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
 
 fn main() {
-    let subject1 = AlwaysEqual;
-    let subject2 = AlwaysEqual;
-    let subject3 = AlwaysEqual2;
-    println!("subject1: {:?}", subject1);
-    println!("subject2: {:?}", subject2);
-    println!("subject3: {:?}", subject3);
-    println!("subject1 == subject2: {}", subject1 == subject2);
-    // println!("subject1 == subject3: {}", subject1 == subject3); // compile error
+    let rect1 = Rectangle { width: 30, height: 50 };
+
+    println!("The area of the rect angle is {} square pixels", area(&rect1))
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
