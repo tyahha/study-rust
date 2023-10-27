795fde4f476c1b918961216bc7db541f2c366cfa
toshinori.sato <tyahhahha@gmail.com>
2023-10-27 22:20:25 +0900

    Using option sample

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,4 @@
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-}
-
 fn main() {
-    let rect1 = Rectangle { width: 30, height: 50 };
-
-    println!("The area of the rectangle is {}", rect1.area());
+    let i1: Option<i8> = Some(3);
+    println!("{:?}", i1);
 }
