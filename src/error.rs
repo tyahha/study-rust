51dbb2a686d4c2999c4e5cedc4341f7b1cfbef6e
toshinori.sato <tyahhahha@gmail.com>
2023-10-31 22:11:05 +0900

    panic

---
+++ src/error.rs
@@ -0,0 +1,4 @@
+pub fn error_main() {
+    panic!("crash and burn");
+    println!("unreachable");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 mod collection;
+mod error;
 
 fn main() {
     println!("--- 8. Common collections");
     collection::collection_main();
+
+    println!("--- 9. Error handling");
+    error::error_main();
 }
