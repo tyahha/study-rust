57d6de7758e859c220fbb4ee9d999384417e5fb3
toshinori.sato <tyahhahha@gmail.com>
2023-11-10 22:25:27 +0900

    Cacher with other value

--- src/closure.rs
+++ src/closure.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
 
@@ -6,7 +7,7 @@ where
     T: Fn(u32) -> u32,
 {
     calculation: T,
-    value: Option<u32>,
+    values: HashMap<u32, u32>,
 }
 
 impl<T> Cacher<T>
@@ -16,16 +17,16 @@ where
     fn new(calculation: T) -> Cacher<T> {
         Cacher {
             calculation,
-            value: None,
+            values: HashMap::new(),
         }
     }
 
     fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
+        match self.values.get(&arg) {
+            Some(&v) => v,
             None => {
                 let v = (self.calculation)(arg);
-                self.value = Some(v);
+                self.values.insert(arg, v);
                 v
             }
         }
@@ -59,4 +60,23 @@ pub fn closure_main() {
     let simulated_random_number = 7;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
+}
+
+#[cfg(test)]
+mod cacher_tests {
+    use super::*;
+    #[test]
+    fn test_cacher1() {
+        let mut cacher = Cacher::new(|v| v);
+        let v1 = cacher.value(1);
+        assert_eq!(v1, 1);
+    }
+
+    #[test]
+    fn test_cacher2() {
+        let mut cacher = Cacher::new(|v| v);
+        cacher.value(1);
+        let v2 = cacher.value(2);
+        assert_eq!(v2, 2);
+    }
 }
