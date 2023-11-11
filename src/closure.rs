5eb05e7f2ab19bfaa1c161bf788f8fb2d850bf34
toshinori.sato <tyahhahha@gmail.com>
2023-11-11 23:05:00 +0900

    capture variable

--- src/closure.rs
+++ src/closure.rs
@@ -91,3 +91,14 @@ mod cacher_tests {
         assert_eq!(arg, v);
     }
 }
+
+#[cfg(test)]
+mod closure_test {
+    #[test]
+    fn test1() {
+        let x = 4;
+        let equal_to_x = |z| z == x;
+        let y = 4;
+        assert!(equal_to_x(y));
+    }
+}
