791a730fe777be0054053fee08a0d93a5079cf43
toshinori.sato <tyahhahha@gmail.com>
2023-11-12 21:39:40 +0900

    iterator sum

--- src/iterator.rs
+++ src/iterator.rs
@@ -6,12 +6,22 @@ pub fn iterator_main() {
     }
 }
 
-#[test]
-fn iterator_test() {
-    let v = vec![1, 2, 3];
-    let mut it = v.iter();
-    assert_eq!(it.next(), Some(&1));
-    assert_eq!(it.next(), Some(&2));
-    assert_eq!(it.next(), Some(&3));
-    assert_eq!(it.next(), None);
+#[cfg(test)]
+mod iterator_test {
+    #[test]
+    fn iterator_test() {
+        let v = vec![1, 2, 3];
+        let mut it = v.iter();
+        assert_eq!(it.next(), Some(&1));
+        assert_eq!(it.next(), Some(&2));
+        assert_eq!(it.next(), Some(&3));
+        assert_eq!(it.next(), None);
+    }
+
+    fn sum_test() {
+        let v = vec![1, 2, 3];
+        let it = v.iter();
+        let s: i32 = it.sum();
+        assert_eq!(s, 6);
+    }
 }
