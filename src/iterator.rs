758c82a4c56c481da03b1ed1c3c0f139a43ec1c7
toshinori.sato <tyahhahha@gmail.com>
2023-11-12 22:01:00 +0900

    impl Iterator

--- src/iterator.rs
+++ src/iterator.rs
@@ -37,3 +37,34 @@ mod iterator_test {
         assert_eq!(v2, [10, 10]);
     }
 }
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+#[test]
+fn counter_test() {
+    let c = Counter::new();
+    let v: Vec<_> = c.collect();
+    assert_eq!(v, [1, 2, 3, 4, 5]);
+}
